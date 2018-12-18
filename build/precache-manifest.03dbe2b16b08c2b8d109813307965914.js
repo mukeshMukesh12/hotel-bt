adbb6d86a2c292f511d3fdfbd12b99b709701fc9
anuj <anuj.k@gai.co.in>
2018-12-18 10:35:50 +0530

    Working version: Adding build file for Hetel page

---
+++ build/precache-manifest.03dbe2b16b08c2b8d109813307965914.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "43a3d43c0f3989596d64e1330967cda5",
+    "url": "/static/media/bhutanbanner.43a3d43c.jpg"
+  },
+  {
+    "revision": "fdf630512fecc1f202dd",
+    "url": "/static/css/main.1aa99bbd.chunk.css"
+  },
+  {
+    "revision": "f0e60a5f093ed282dd294bfb370c506a",
+    "url": "/static/media/restu.f0e60a5f.jpg"
+  },
+  {
+    "revision": "8c17ae2a89955da8a526",
+    "url": "/static/js/1.8c17ae2a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb4fdcb37873aa513b302cc64ba44c8b",
+    "url": "/static/media/4.bb4fdcb3.jpg"
+  },
+  {
+    "revision": "02f7baf4dc62e8a3f00d6e8fbd73b033",
+    "url": "/static/media/1.02f7baf4.jpg"
+  },
+  {
+    "revision": "8347bb8266298bc1b4d786c737434bb9",
+    "url": "/static/media/6.8347bb82.jpg"
+  },
+  {
+    "revision": "19044574789d412f9546147ce54de89b",
+    "url": "/static/media/5.19044574.jpg"
+  },
+  {
+    "revision": "9b39e9e294a090065ad67d3f4d40de1e",
+    "url": "/static/media/8.9b39e9e2.jpg"
+  },
+  {
+    "revision": "8815d9ca64de3c37aea8d1b55e12575f",
+    "url": "/static/media/delux.8815d9ca.jpg"
+  },
+  {
+    "revision": "607964b57fb8c94871506e394caefa25",
+    "url": "/static/media/logo.607964b5.png"
+  },
+  {
+    "revision": "fdf630512fecc1f202dd",
+    "url": "/static/js/main.fdf63051.chunk.js"
+  },
+  {
+    "revision": "9b39e9e294a090065ad67d3f4d40de1e",
+    "url": "/static/media/7.9b39e9e2.jpg"
+  },
+  {
+    "revision": "27c0d317fe945f73bf30904abe822d85",
+    "url": "/static/media/9.27c0d317.jpg"
+  },
+  {
+    "revision": "823beb5876623d2c7ce5db375427aca7",
+    "url": "/static/media/2.823beb58.jpeg"
+  },
+  {
+    "revision": "5f591d2924507eff7afa94796f70ceeb",
+    "url": "/static/media/15.5f591d29.jpg"
+  },
+  {
+    "revision": "5f591d2924507eff7afa94796f70ceeb",
+    "url": "/static/media/16.5f591d29.jpg"
+  },
+  {
+    "revision": "bff12741441406488cd4c1dff0cfbd96",
+    "url": "/static/media/17.bff12741.jpg"
+  },
+  {
+    "revision": "c9757a793848606f2e8909796c9973f5",
+    "url": "/static/media/f.c9757a79.svg"
+  },
+  {
+    "revision": "03b99fb3593e60f82c1d6063d0feda0f",
+    "url": "/static/media/t.03b99fb3.svg"
+  },
+  {
+    "revision": "83996a326a4a86bf8698bd5e1e91b25c",
+    "url": "/static/media/l.83996a32.svg"
+  },
+  {
+    "revision": "8c17ae2a89955da8a526",
+    "url": "/static/css/1.605a28ac.chunk.css"
+  },
+  {
+    "revision": "8c2616d6388c80fc0a160d1775d080d0",
+    "url": "/index.html"
+  }
+];
